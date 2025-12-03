ca12226ea28f4205401bdb0e4b6389746ea67cfb
abhilashaproject <abhilasha.varshney@gmail.com>
2025-12-03 10:26:00 +0530

    new changes

--- src/test/java/com/qa/tests/NewTest.java
+++ src/test/java/com/qa/tests/NewTest.java
@@ -1,7 +1,11 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
 public class NewTest
   {
    @Test
-    public void Test
+    public void test123()
      {
      System.out.println("dfds");
    }
